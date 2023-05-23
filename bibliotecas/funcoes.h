9f2fe35a10e2ae5fbc203e0e05d77fdde19d59d8
RafaelNogueiraXD <rafa10_nogueira@hotmail.com>
2023-05-23 17:46:36 -0300

    vsLinux

--- bibliotecas/funcoes.h
+++ bibliotecas/funcoes.h
@@ -4,13 +4,13 @@
 #define MAX_TAM_NOME 100
 #define MAX_NOMES 200
 #define MAX_LINE_SIZE 1024
+#define MAX_LINE_LENGTH 1000
+#define MAX_COLUMNS 10
 int comparar_nomes(const void* a, const void* b) {  
     const char** nome1 = (const char**)a;
     const char** nome2 = (const char**)b;
     return strcmp(*nome1, *nome2);
 }
-
-
 void lerNomes(char nomes[MAX_NOMES][MAX_TAM_NOME], int *quantidade, char *nome) {
     if (*quantidade < MAX_NOMES) {
         strcpy(nomes[*quantidade], nome);
@@ -90,4 +90,66 @@ void liberarArvore(No* raiz) {
         free(raiz->notes);
         free(raiz);
     }
+}
+void menu(){
+    int op;
+    printf("\nOpcoes: \n");
+    printf("\n\t 0 - Fechar programa");
+    printf("\n\t 1 - Cadastrar novos itens.");
+    printf("\n\t 2 - Atualizar determinado item");
+    printf("\n\t 3 - Remover item");
+    printf("\n\t 4 - Consultar por item");
+    printf("\n\t 5 - Vizualizar um item");
+    printf("\n\t 6 - exportar informacoes");
+ 
+}
+
+No* lerArquivo(char* nomeArquivo, char*** VetorNomes, int* total) {
+    FILE* file = fopen(nomeArquivo, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return NULL;
+    }
+    printf("\n Achamos o arquivo!\n");
+
+    No* raiz = NULL;
+    char line[MAX_LINE_LENGTH];
+    char *token;
+    char *columns[MAX_COLUMNS];
+    int columnCount = 0;
+    int troca = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        token = strtok(line, ",");
+        columnCount = 0;
+
+        while (token != NULL && columnCount < MAX_COLUMNS) {
+            columns[columnCount] = token;
+            columnCount++;
+            token = strtok(NULL, ",");
+        }
+
+        *VetorNomes = realloc(*VetorNomes, (*total + 1) * sizeof(char*));
+        (*VetorNomes)[*total] = malloc(strlen(columns[0]) + 1);
+        strcpy((*VetorNomes)[*total], columns[0]);
+        (*total)++;
+
+        if (strcmp(columns[0], "Acid Drop") == 0)
+            troca = 1;
+
+        if (troca == 0)
+            raiz = inserirNo(raiz, columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6]);
+        else
+            raiz = inserirNo(raiz, columns[0], "Vazio", "Vazio", columns[1], columns[3], columns[4], columns[5]);
+    }
+
+    fclose(file);
+    return raiz;
+}
+
+void liberarMemoria(char** VetorNomes, int total) {
+    for (int i = 0; i < total; i++) {
+        free(VetorNomes[i]);
+    }
+    free(VetorNomes);
 }
--- mainReduct.c
+++ mainReduct.c
@@ -3,76 +3,66 @@
 #include <string.h>
 #include "bibliotecas/funcoes.h"
 #include "bibliotecas/funcoesAtv.h"
-#define MAX_LINE_LENGTH 1000
-#define MAX_COLUMNS 10
-
-No* lerArquivo(char* nomeArquivo, char*** VetorNomes, int* total) {
-    FILE* file = fopen(nomeArquivo, "r");
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return NULL;
-    }
-    printf("\n Achamos o arquivo!\n");
-
-    No* raiz = NULL;
-    char line[MAX_LINE_LENGTH];
-    char *token;
-    char *columns[MAX_COLUMNS];
-    int columnCount = 0;
-    int troca = 0;
-
-    while (fgets(line, sizeof(line), file) != NULL) {
-        token = strtok(line, ",");
-        columnCount = 0;
-
-        while (token != NULL && columnCount < MAX_COLUMNS) {
-            columns[columnCount] = token;
-            columnCount++;
-            token = strtok(NULL, ",");
-        }
-
-        *VetorNomes = realloc(*VetorNomes, (*total + 1) * sizeof(char*));
-        (*VetorNomes)[*total] = malloc(strlen(columns[0]) + 1);
-        strcpy((*VetorNomes)[*total], columns[0]);
-        (*total)++;
-
-        if (strcmp(columns[0], "Acid Drop") == 0)
-            troca = 1;
-
-        if (troca == 0)
-            raiz = inserirNo(raiz, columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6]);
-        else
-            raiz = inserirNo(raiz, columns[0], "Vazio", "Vazio", columns[1], columns[3], columns[4], columns[5]);
-    }
-
-    fclose(file);
-    return raiz;
-}
-
-void liberarMemoria(char** VetorNomes, int total) {
-    for (int i = 0; i < total; i++) {
-        free(VetorNomes[i]);
-    }
-    free(VetorNomes);
-}
 
 int main() {
     No* raiz = NULL;
     char** VetorNomes = NULL;
-    int total = 0;
+    int total = 0, opcao;
 
     raiz = lerArquivo("../dados/AtariCSV.csv", &VetorNomes, &total);
     if (raiz == NULL) {
         liberarMemoria(VetorNomes, total);
         return 1;
     }
-    inserirNo2(raiz, "teste", NULL, NULL, NULL, NULL, NULL, NULL);
-    if(buscarNo(raiz,"teste") != NULL)
-        mostraBusca(buscarNo(raiz,"teste"));
-    removerNo(raiz,"teste");  
-    if(buscarNo(raiz,"teste") != NULL)
-        mostraBusca(buscarNo(raiz,"teste"));
-    else printf("\nNenhum dado foi encontrado!");
+
+    do {
+        menu();
+        scanf("%d", &opcao);
+        getchar(); // Limpar o caractere de nova linha deixado por scanf
+
+        switch (opcao) {
+        case 0:
+            printf("\nFechando programa.\n");
+            break;
+        case 1: 
+            char newGame[MAX_TAM_NOME];
+            printf("\nDigite um nome: ");
+            fgets(newGame, MAX_TAM_NOME, stdin);
+            size_t length = strlen(newGame);
+            if (newGame[length - 1] == '\n') {
+                newGame[length - 1] = '\0';
+            }
+            inserirNo2(raiz, newGame, "vazio", "vazio", "vazio", "vazio", "vazio", "vazio");
+            break;
+        
+        case 2:
+            // Case 2
+            break;
+        case 3:
+            // Case 3
+            removerNo(raiz, "teste");
+            break;
+        case 4:
+            if (buscarNo(raiz, "teste") != NULL)
+                mostraBusca(buscarNo(raiz, "teste"));
+            break;
+        case 5:
+            // Case 5
+            break;
+        case 6:
+            // Case 6
+            break;
+        default:
+            printf("\nOpcao invalida!\n");
+            break;
+        }
+    } while (opcao != 0);
+
+
+    if (buscarNo(raiz, "teste") != NULL)
+        mostraBusca(buscarNo(raiz, "teste"));
+    else
+        printf("\nNenhum dado foi encontrado!\n");
 
     liberarArvore(raiz);
     liberarMemoria(VetorNomes, total);
